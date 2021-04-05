5adedc6906641cf325a8725cad8deb7e35881ea3
tbriggs1 <tombriggs93@protonmail.com>
2021-04-05 15:38:05 +0100

    API call made

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,54 +1,82 @@
-import React, { Component } from 'react'
+import React, { Component,useEffect,useState } from 'react'
 import axios from 'axios';
-import Suggestions from './Suggestions';
+// import Suggestions from './Suggestions';
 
-// const { API_KEY } = process.env
-// const API_URL = 'http://api.musicgraph.com/api/v2/artist/suggest'
+// // const { API_KEY } = process.env
+// // const API_URL = 'http://api.musicgraph.com/api/v2/artist/suggest'
 
-class Search extends Component {
-  state = {
-    query: '',
-    results: []
-  }
 
-  getInfo = () => {
-    axios.get(`http://127.0.0.1:8000/api/skills/`)
-      .then(({ data }) => {
-        this.setState({
-          results: data.data
-        }).then(
-            console.log(this.results)
-        )
-      })
-  }
+const Search = () => {
+
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        axios.get(`http://127.0.0.1:8000/api/skills/`)
+              .then((response) => {
+                  setData(response.data[0].title);
+                //   console.log(response.data.data);
+                //   console.log(response.data[0].title);
+                //   console.log(response.data[0])
+                //   console.log(response.title);
+                  console.log(data);
+              })
+    })
+
+    return(
+        <div>
+            <form>
+                <input
+                placeholder="Search for..."/>
+            </form>
+        </div>
+    )
+}
+
+export default Search;
+// class Search extends Component {
+//   state = {
+//     query: '',
+//     results: []
+//   }
+
+//   getInfo = () => {
+//     axios.get(`http://127.0.0.1:8000/api/skills/`)
+//       .then(({ data }) => {
+//         this.setState({
+//           results: data.data
+//         }).then(
+//             console.log(this.results)
+//         )
+//       })
+//   }
 
   
 
-  handleInputChange = () => {
-    this.setState({
-      query: this.search.value
-    }, () => {
-      if (this.state.query && this.state.query.length > 1) {
-        if (this.state.query.length % 2 === 0) {
-          this.getInfo()
-        }
-      } else if (!this.state.query) {
-      }
-    })
-  }
+//   handleInputChange = () => {
+//     this.setState({
+//       query: this.search.value
+//     }, () => {
+//       if (this.state.query && this.state.query.length > 1) {
+//         if (this.state.query.length % 2 === 0) {
+//           this.getInfo()
+//         }
+//       } else if (!this.state.query) {
+//       }
+//     })
+//   }
 
-  render() {
-    return (
-      <form>
-        <input
-          placeholder="Search for..."
-          ref={input => this.search = input}
-          onChange={this.handleInputChange}
-        />
-        <Suggestions results={this.state.results} />
-      </form>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <form>
+//         <input
+//           placeholder="Search for..."
+//           ref={input => this.search = input}
+//           onChange={this.handleInputChange}
+//         />
+//         <Suggestions results={this.state.results} />
+//       </form>
+//     )
+//   }
+// }
 
-export default Search
+// export default Search
